6b2ccb2d363f3f6b34a69619281074e75b1457e3
Loc Tan Vo <loc.tan.vo@gmail.com>
2016-11-06 10:06:24 +0100

    Merged from ltv-1

--- searchapps/scripts/app-bundle.js
+++ searchapps/scripts/app-bundle.js
@@ -100,12 +100,13 @@ define('resources/index',["exports"], function (exports) {
   exports.configure = configure;
   function configure(config) {}
 });
-define('services/search-api',['exports'], function (exports) {
+define('services/search-api',['exports', 'aurelia-http-client'], function (exports, _aureliaHttpClient) {
     'use strict';
 
     Object.defineProperty(exports, "__esModule", {
         value: true
     });
+    exports.SearchApi = undefined;
 
     function _classCallCheck(instance, Constructor) {
         if (!(instance instanceof Constructor)) {
@@ -113,36 +114,43 @@ define('services/search-api',['exports'], function (exports) {
         }
     }
 
-    var searchResults = [{
-        name: 'Nissefar',
-        brewery: 'Nøgne Ø',
-        type: 'Porter'
-    }, {
-        name: 'Bestefar',
-        brewery: 'Nøgne Ø',
-        type: 'Porter'
-    }, {
-        name: 'Tiger Trippel',
-        brewery: 'Nøgne Ø',
-        type: 'Tripel Ale'
-    }];
-
-    var suggestions = ['Nissefar', 'Bestefar', 'Oldefar'];
-
     var SearchApi = exports.SearchApi = function () {
         function SearchApi() {
             _classCallCheck(this, SearchApi);
+
+            this.httpClient = new _aureliaHttpClient.HttpClient().configure(function (x) {
+                x.withBaseUrl('https://azuresearchfree.search.windows.net/indexes/beersv1/docs/search?api-version=2015-02-28');
+                x.withHeader('api-key', '5655AB55C4E55DBE67C691F376482D8C');
+            });
         }
 
         SearchApi.prototype.search = function search(query) {
+            var _this = this;
+
             return new Promise(function (resolve) {
-                resolve(searchResults);
+                _this.httpClient.post("", {
+                    "count": true,
+                    "search": query
+                }).then(function (result) {
+                    var jsonResult = JSON.parse(result.response);
+                    resolve({
+                        count: jsonResult["@odata.count"],
+                        results: jsonResult["value"].map(function (x) {
+                            return {
+                                name: x.name,
+                                style: x.style,
+                                brewery: x.breweries[0]
+                            };
+                        }),
+                        raw: jsonResult
+                    });
+                });
             });
         };
 
         SearchApi.prototype.suggest = function suggest(query) {
             return new Promise(function (resolve) {
-                resolve(suggestions);
+                resolve(['nissefar', 'bestefar', 'oldefar']);
             });
         };
 
@@ -163,49 +171,57 @@ define('simple/simple',['exports', 'services/search-api'], function (exports, _s
         }
     }
 
-    var _class, _temp;
+    var _class2, _temp;
 
-    var Simple = exports.Simple = (_temp = _class = function () {
+    var Simple = exports.Simple = (_temp = _class2 = function () {
         function Simple(api) {
             _classCallCheck(this, Simple);
 
             this.api = api;
             this.results = [];
             this.queryText = '';
+            this.rawResult = '';
+            this.count = null;
+            this.rawJson = '';
         }
 
         Simple.prototype.search = function search() {
             var _this = this;
 
-            this.api.search(this.queryText).then(function (results) {
-                return _this.results = results;
+            this.api.search(this.queryText).then(function (x) {
+                _this.count = x.count;
+                _this.results = x.results;
+                _this.rawResult = JSON.stringify(x.raw, null, 4);
             });
         };
 
         Simple.prototype.clear = function clear() {
             this.results = [];
+            this.queryText = '';
+            this.rawResult = '';
+            this.count = null;
         };
 
         Simple.prototype.attached = function attached() {
-            var _api = this.api;
+            var _class = this;
             $(".search-input").autocomplete({
                 source: function source(request, response) {
-                    _api.suggest(request.term).then(function (results) {
+                    _class.api.suggest(request.term).then(function (results) {
                         return response(results);
                     });
                 },
                 minLength: 2,
                 select: function select(event, ui) {
-                    console.log("Selected: " + ui.item.value + " aka " + ui.item.id);
+                    _class.queryText = ui.item.value;
                 }
             });
         };
 
         return Simple;
-    }(), _class.inject = [_searchApi.SearchApi], _temp);
+    }(), _class2.inject = [_searchApi.SearchApi], _temp);
 });
 define('text!app.html', ['module'], function(module) { module.exports = "<template>\n  <router-view></router-view>\n</template>"; });
-define('text!simple/simple.css', ['module'], function(module) { module.exports = ".search-container {\n    margin-top: 20vh;\n}\n\n.has-results {\n    margin-top: 10px !important;\n}\n\n.search-title {\n    font-family: 'Montserrat';\n    font-size: 5.5em;\n    margin: 20px;\n}\n\n.search-title-container::after {\n    content: \"Norway\";\n    font-size: 16px;\n    font-family: \"Roboto\";\n    color: #4285f4;\n    position: relative;\n    top: -40px;\n    right: -110px;\n}\n\n.search-title span {\n    margin: -14px;\n}\n\n.search-input {\n    height: 2.5em;\n}\n\n.search-button-row {\n    margin-top: 20px;\n    width: 400px;\n    margin: 30px auto;\n    text-align: center;\n}\n\n.search-button {\n    font-size: 12px;\n    margin: 0 2px;\n}\n\n.g-blue {\n    color: #4C90F5;\n}\n\n.g-red {\n    color: #ED4D3C;\n}\n\n.g-yellow {\n    color: #FBC402;\n}\n\n.g-green {\n    color: #3BB15D;\n}\n\n.btn-primary {\n    background-color: #F5F5F5;\n    color: #808080;\n    border: none;\n    font-weight: bold;\n    border-radius: 2px;\n}\n\n.brand-img {\n    width: 100%;\n}\n\n.result {\n    padding: 0;\n}"; });
+define('text!simple/simple.css', ['module'], function(module) { module.exports = ".search-container {\n    margin-top: 20vh;\n}\n\n.has-results {\n    margin-top: 10px !important;\n}\n\n.search-title {\n    font-family: 'Montserrat';\n    font-size: 5.5em;\n    margin: 20px;\n}\n\n.search-title-container::after {\n    content: \"Norway\";\n    font-size: 16px;\n    font-family: \"Roboto\";\n    color: #4285f4;\n    position: relative;\n    top: -40px;\n    right: -110px;\n}\n\n.search-title span {\n    margin: -14px;\n}\n\n.search-input {\n    height: 2.5em;\n}\n\n.search-button-row {\n    margin-top: 20px;\n    width: 400px;\n    margin: 30px auto;\n    text-align: center;\n}\n\n.search-button {\n    font-size: 12px;\n    margin: 0 2px;\n}\n\n.g-blue {\n    color: #4C90F5;\n}\n\n.g-red {\n    color: #ED4D3C;\n}\n\n.g-yellow {\n    color: #FBC402;\n}\n\n.g-green {\n    color: #3BB15D;\n}\n\n.btn-primary {\n    background-color: #F5F5F5;\n    color: #808080;\n    border: none;\n    font-weight: bold;\n    border-radius: 2px;\n}\n\n.brand-img {\n    width: 100%;\n}\n\n.result {\n    padding: 0;\n}\n\n.raw-result {\n    background-color: whitesmoke;\n    font-size: 0.8em;\n}\n\n.raw-result-trigger {\n    font-size: 0.8em;\n}"; });
 define('text!home/home.html', ['module'], function(module) { module.exports = "<template>\n    <h1>At Home</h1>\n</template>"; });
-define('text!simple/simple.html', ['module'], function(module) { module.exports = "<template>\n\t<require from=\"./simple.css\"></require>\n\n\t<div class=\"container\">\n\t\t<div class=\"search-container ${results.length > 0 ? 'has-results' : ''}\">\n\t\t\t<div class=\"text-xs-center search-title-container\">\n\t\t\t\t<h1 class=\"search-title\">\n\t\t\t\t\t<span class=\"g-blue\">S</span>\n\t\t\t\t\t<span class=\"g-red\">e</span>\n\t\t\t\t\t<span class=\"g-yellow\">a</span>\n\t\t\t\t\t<span class=\"g-blue\">r</span>\n\t\t\t\t\t<span class=\"g-green\">c</span>\n\t\t\t\t\t<span class=\"g-red\">h</span>\n\t\t\t\t</h1>\n\t\t\t</div>\n\t\t\t<form submit.delegate=\"search()\">\n\t\t\t\t<div class=\"row\">\n\t\t\t\t\t<input type=\"text\" value.bind=\"queryText\" class=\"search-input col-md-6 col-xs-8 offset-xs-2 offset-md-3\">\n\t\t\t\t</div>\n\t\t\t\t<div class=\"search-button-row\">\n\t\t\t\t\t<input type=\"submit\" value=\"Search Search\" class=\"btn btn-primary search-button\">\n\t\t\t\t\t<input type=\"button\" click.delegate=\"clear()\" value=\"I'm Feeling Tired\" class=\"btn btn-primary search-button\">\n\t\t\t\t</div>\n\n\t\t\t</form>\n\t\t</div>\n\t\t<div class=\"row\">\n\t\t\t<div repeat.for=\"result of results\" class=\"card col-md-4 result\">\n\t\t\t\t<img class=\"card-img-top brand-img\" src=\"https://unsplash.it/300/200?image=876\" alt=\"Card image cap\">\n\t\t\t\t<div class=\"card-block\">\n\t\t\t\t\t<h4 class=\"card-title\">${result.name}</h4>\n\t\t\t\t\t<p class=\"card-text\">${result.brewery}</p>\n\t\t\t\t\t<p class=\"card-text\">${result.type}</p>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t</div>\n\t</div>\n\n</template>"; });
+define('text!simple/simple.html', ['module'], function(module) { module.exports = "<template>\n\t<require from=\"./simple.css\"></require>\n\n\t<div class=\"container\">\n\t\t<div class=\"search-container ${results.length > 0 ? 'has-results' : ''}\">\n\t\t\t<div class=\"text-xs-center search-title-container\">\n\t\t\t\t<h1 class=\"search-title\">\n\t\t\t\t\t<span class=\"g-blue\">S</span>\n\t\t\t\t\t<span class=\"g-red\">e</span>\n\t\t\t\t\t<span class=\"g-yellow\">a</span>\n\t\t\t\t\t<span class=\"g-blue\">r</span>\n\t\t\t\t\t<span class=\"g-green\">c</span>\n\t\t\t\t\t<span class=\"g-red\">h</span>\n\t\t\t\t</h1>\n\t\t\t</div>\n\t\t\t<form submit.delegate=\"search()\">\n\t\t\t\t<div class=\"row\">\n\t\t\t\t\t<input type=\"text\" value.bind=\"queryText\" class=\"search-input col-md-6 col-xs-8 offset-xs-2 offset-md-3\">\n\t\t\t\t</div>\n\t\t\t\t<div class=\"search-button-row\">\n\t\t\t\t\t<input type=\"submit\" value=\"Search Search\" class=\"btn btn-primary search-button\">\n\t\t\t\t\t<input type=\"button\" click.delegate=\"clear()\" value=\"I'm Feeling Tired\" class=\"btn btn-primary search-button\">\n\t\t\t\t</div>\n\t\t\t</form>\n\t\t</div>\n\t\t\t\n\t\t<div class=\"row\" if.bind=\"rawResult\">\n\t\t\t<p>\n\t\t\t\tFound <span class=\"tag tag-info\">${count}</span>, displaying <span class=\"tag tag-info\">${results.length}</span>. \n\t\t\t\t<a class=\"text-muted raw-result-trigger\" data-toggle=\"collapse\" href=\"#rawResult\" aria-expanded=\"false\" aria-controls=\"collapseExample\">\n\t\t\t\t\tRaw result\t\t\t\n\t\t\t\t</a>\n\n\t\t\t</p>\n\t\t\t<div class=\"collapse\" id=\"rawResult\">\n\t\t\t\t<div class=\"card card-block raw-result\">\n\t\t\t\t\t<pre>${rawResult}</pre>\t\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t\t<div repeat.for=\"result of results\" class=\"card col-md-12 result\">\n\t\t\t\t<div class=\"card-block\">\n\t\t\t\t\t<h4 class=\"card-title\">${result.name}</h4>\n\t\t\t\t\t<p class=\"card-text\">${result.style}</p>\n\t\t\t\t\t<p class=\"card-text\">${result.brewery}</p>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t</div>\n\t</div>\n\n</template>"; });
 //# sourceMappingURL=app-bundle.js.map
