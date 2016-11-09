20069cc764fa63ffd1aa70a5eeb8f8fd85bc811a
Loc Tan Vo <loc.tan.vo@gmail.com>
2016-11-09 19:03:40 +0100

    Finally getting .withParams to work with http builder

--- searchapps/src/services/search-api.js
+++ searchapps/src/services/search-api.js
@@ -5,15 +5,16 @@ export class SearchApi {
     constructor() {
         this.httpClient = new HttpClient()
             .configure(x => {
-                x.withBaseUrl('https://azuresearchfree.search.windows.net/indexes/beersv1/docs/search?api-version=2015-02-28')
-                x.withHeader('api-key', '5655AB55C4E55DBE67C691F376482D8C');
+                x.withBaseUrl("https://azuresearchfree.search.windows.net/indexes/beersv1/docs")
+                x.withParams({ "api-version": "2015-02-28" });
+                x.withHeader("api-key", "5655AB55C4E55DBE67C691F376482D8C");
             })
     }
 
     search(query) {
         return new Promise(resolve => {
             this.httpClient
-                .post("", {
+                .post("/search", {
                     "count": true,
                     "search": query
                 })
