75b8cab91b941ff9b64a5404603950e4ac17d905
Loc Tan Vo <loc.tan.vo@gmail.com>
2016-11-10 00:14:39 +0100

    Started styling the Finn lookalike site

--- searchapps/src/app.js
+++ searchapps/src/app.js
@@ -4,7 +4,8 @@ export class App {
         config.title = 'Azure Search Demo';
         config.map([
             { route: ['', 'home'], name: 'home', moduleId: 'home/home', title: 'Home' },
-            { route: 'simple', name: 'simple', moduleId: 'simple/simple', title: 'Simple', nav: true }
+            { route: 'simple', name: 'simple', moduleId: 'simple/simple', title: 'Simple', nav: true },
+            { route: 'facets', name: 'facets', moduleId: 'facets/facets', title: 'Faceted Search', nav: true }
         ]);
     }
 }
