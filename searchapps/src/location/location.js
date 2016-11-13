f0d4a273cb9ba49cf59e7b54601c0f10cb565cd6
Loc Tan Vo <loc.tan.vo@gmail.com>
2016-11-13 10:04:19 +0100

    Superhacky, but map is now displayedgit status

--- searchapps/src/location/location.js
+++ searchapps/src/location/location.js
@@ -1,3 +1,14 @@
 export class Location {
+    attached() {
+        //initMap();
 
+        let scriptURL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCpc-ixRTVtC3qx-55OvECcX01bEw9siCA&callback=initMap";
+        let scriptElement = document.createElement('script');
+        scriptElement.src = scriptURL;
+        scriptElement.onload = () => {
+            // console.log($('#map'));
+        };
+
+        document.querySelector('body').appendChild(scriptElement);
+    }
 }
