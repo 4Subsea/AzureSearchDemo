c6338f1948cf6fcec8e84998c443f1fee915fc3d
Loc Tan Vo <loc.tan.vo@gmail.com>
2016-11-13 10:28:51 +0100

    Added a style

--- searchapps/src/location/location.js
+++ searchapps/src/location/location.js
@@ -1,4 +1,6 @@
 export class Location {
+
+
     attached() {
         this.initializeMap();
     }
@@ -6,7 +8,8 @@ export class Location {
     onMapInitialized() {
         this.map = new google.maps.Map(document.getElementById('map'), {
             center: { lat: -34.397, lng: 150.644 },
-            zoom: 8
+            zoom: 8,
+            styles: [{ "featureType": "administrative", "elementType": "labels.text.fill", "stylers": [{ "color": "#444444" }] }, { "featureType": "landscape", "elementType": "all", "stylers": [{ "color": "#f2f2f2" }] }, { "featureType": "poi", "elementType": "all", "stylers": [{ "visibility": "off" }] }, { "featureType": "poi.business", "elementType": "geometry.fill", "stylers": [{ "visibility": "on" }] }, { "featureType": "road", "elementType": "all", "stylers": [{ "saturation": -100 }, { "lightness": 45 }] }, { "featureType": "road.highway", "elementType": "all", "stylers": [{ "visibility": "simplified" }] }, { "featureType": "road.arterial", "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, { "featureType": "transit", "elementType": "all", "stylers": [{ "visibility": "off" }] }, { "featureType": "water", "elementType": "all", "stylers": [{ "color": "#b4d4e1" }, { "visibility": "on" }] }]
         });
 
         // var map;
