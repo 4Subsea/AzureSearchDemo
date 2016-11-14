85e46648ff8429b8f9a70620278bb28fa0e2b016
Loc Tan Vo <loc.tan.vo@gmail.com>
2016-11-15 00:56:36 +0100

    Removed unused methods

--- searchapps/src/services/search-api.js
+++ searchapps/src/services/search-api.js
@@ -14,100 +14,12 @@ export class SearchApi {
             })
     }
 
-    search(query) {
-        return new Promise(resolve => {
-            this.httpClient
-                .post("/search", {
-                    count: true,
-                    search: query
-                })
-                .then(result => {
-                    let jsonResult = JSON.parse(result.response);
-                    resolve({
-                        count: jsonResult["@odata.count"],
-                        results: jsonResult["value"].map(x => {
-                            return {
-                                name: x.name,
-                                description: x.description,
-                                label: x.labelmediumimage,
-                                style: x.stylename,
-                                brewery: x.breweries[0]
-                            }
-                        })
-                    });
-                });
-        });
-    }
-
-    suggest(query) {
-        return new Promise(resolve => {
-            this.httpClient
-                .post("/suggest", {
-                    search: query,
-                    suggesterName: "suggestBeerName",
-                    highlightPreTag: "<strong>",
-                    highlightPostTag: "</strong>"
-                })
-                .then(result => {
-                    var results = JSON.parse(result.response).value;
-                    resolve(results.map(x => x["@search.text"]));
-                });
-        })
-    }
-
-    faceted(query, filter) {
-        return new Promise(resolve => {
-            this.httpClient
-                .post("/search", {
-                    facets: [
-                        "stylename",
-                        "abv,values:5|10|15",
-                        "breweries",
-                        "created,interval:year"
-                    ],
-                    search: query,
-                    filter: filter,
-                    count: true
-                })
-                .then(result => {
-                    var jsonResponse = JSON.parse(result.response);
-                    var facets = jsonResponse["@search.facets"];
-                    var mapped = {
-                        facets: {
-                            stylename: facets.stylename,
-                            abv: facets.abv,
-                            breweries: facets.breweries,
-                            created: facets.created.map(x => {
-                                return {
-                                    value: new Date(x.value).getFullYear(),
-                                    count: x.count
-                                }
-                            }),
-                        },
-                        results: jsonResponse.value.map(x => {
-                            return {
-                                name: x.name,
-                                description: x.description,
-                                alcoholPercentage: x.abv,
-                                label: x.labelmediumimage,
-                                style: x.stylename,
-                                brewery: x.breweries[0]
-                            }
-                        }),
-                        count: jsonResponse["@odata.count"],
-                    }
-
-                    resolve(mapped);
-                });
-        })
-    }
-
     nearest(query, location, radiusLimit) {
         let geoDistance = `geo.distance(brewerylocation, geography'POINT(${location.lng} ${location.lat})')`;
-        let filter = "(brewerylocation ne null)";
+        let filter = "";
 
         if (radiusLimit) {
-            filter += ` and (${geoDistance} le ${radiusLimit})`;
+            filter += `(${geoDistance} le ${radiusLimit})`;
         }
 
         return new Promise(resolve => {
